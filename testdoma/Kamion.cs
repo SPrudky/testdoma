f358c2821c65bb9bd4ecf37c73f623868b569c98
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2022-06-06 22:14:05 +0200

    Přidejte soubory projektu.

---
+++ testdoma/Kamion.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testdoma
+{
+    public class Kamion
+    {
+        public int maxcarry { get; set; }
+        public int maxfuel { get; set; }
+        public int consum { get; set; }
+        public int distance { get; set; }
+        public int curentcarry { get; set; }
+        public int curentfuel { get; set; }
+
+        public Kamion()
+        {
+            maxcarry = 2000;
+            curentcarry = 0;
+            maxfuel = 250;
+            curentfuel = 250;
+            distance = 0;
+            consum = 0;
+        }
+        public void Loadcargo()
+        {
+            if (curentcarry == 2000)
+            {
+                curentcarry = 2000;
+            }
+            else if (curentcarry < 2000)
+            {
+                curentcarry += 50;
+            }
+            
+        }
+        public void Unloadcargo()
+        {
+            if (curentcarry == 0)
+            {
+                curentcarry = 0;
+            }
+            else if (curentcarry > 0)
+            {
+                curentcarry -= 50;
+            }
+        }
+        public void Go()
+        {
+            if (curentfuel == 0)
+            {
+                distance += 0;
+                curentfuel = 0;
+                    
+            }
+            else if (curentcarry > 500)
+            {
+                curentfuel -= 30;
+                distance += 20;
+            }
+            else if (curentcarry < 500)
+            {
+                curentfuel -= 20;
+                distance += 20;
+            }
+
+            
+        }
+        public void Consum()
+        {
+            if (curentfuel == 0)
+            {
+                consum = 0;
+            }
+            else if (curentcarry > 500)
+            {
+                consum = 30;
+            }
+            else if (curentcarry<500)
+            {
+                consum = 20;
+            }
+        } 
+        public void Refuel()
+        {
+            curentfuel += 50;
+        }
+    }
+}
---
+++ testdoma/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testdoma
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Kamion kamion1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            kamion1 = new Kamion();
+            Showkamion(kamion1, txtbox1);
+        }
+        public void Showkamion(Kamion vehicle,TextBox text)
+        {
+            text.Text = $"Maxcarry: {vehicle.maxcarry}\n";
+            text.Text = $"Maxfuel: {vehicle.maxfuel}\n";
+            text.Text = $"Curentcarry:{vehicle.curentcarry}\n";
+            text.Text = $"Curentfuel:{vehicle.curentfuel}\n";
+            text.Text = $"Consum:{vehicle.consum}\n";
+            text.Text = $"Distance:{vehicle.distance}\n";
+
+
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            kamion1.Go();
+            kamion1.Consum();
+            Showkamion(kamion1, txtbox1);
+        }
+
+        public void refuel_Click(object sender, RoutedEventArgs e)
+        {
+            kamion1.Refuel();
+            Showkamion(kamion1, txtbox1);
+        }
+
+        public void load_Click(object sender, RoutedEventArgs e)
+        {
+            kamion1.Loadcargo();
+            Showkamion(kamion1, txtbox1);
+        }
+
+        public void unload_Click(object sender, RoutedEventArgs e)
+        {
+            kamion1.Unloadcargo();
+            Showkamion(kamion1, txtbox1);
+        }
+    }
+}
